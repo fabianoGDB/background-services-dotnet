52eda19989561eb04f44cf990036a4f6765fbb36
fabianoGDB <doctorfabi.f@gmail.com>
2025-05-31 23:47:43 -0300

    A002: creating a seccond console app to show the use of workers

---
+++ version-two/HostDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using HostDemo;
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder();
+builder.Services.AddHostedService<TimeService>();
+builder.Services.Configure<HostOptions>(o => {
+    o.StartupTimeout = TimeSpan.FromSeconds(5);
+    o.ServicesStartConcurrently = true;
+});
+IHost host = builder.Build();
+var lifetime = host.Services.GetRequiredService<IHostApplicationLifetime>();
+//lifetime.ApplicationStarted.Register(() => Console.WriteLine("Hello World"));
+await host.RunAsync();
---
+++ version-two/HostDemo/TimeService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HostDemo
+{
+    internal class TimeService(ILogger<TimeService> logger) : BackgroundService
+    {
+        private readonly ILogger<TimeService> _logger = logger;
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _ = Task.Run(async () =>
+            {
+                while (true) 
+                {
+                    cancellationToken.ThrowIfCancellationRequested();
+                    _logger.LogInformation("The Time is {Time}", TimeOnly.FromDateTime(DateTime.UtcNow));
+                    await Task.Delay(2000, cancellationToken);
+                };
+
+            }, cancellationToken);
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (true)
+            {
+                stoppingToken.ThrowIfCancellationRequested();
+                _logger.LogInformation("The Time is {Time}", TimeOnly.FromDateTime(DateTime.UtcNow));
+                await Task.Delay(2000, stoppingToken);
+            };
+        }
+    }
+
+
+}
